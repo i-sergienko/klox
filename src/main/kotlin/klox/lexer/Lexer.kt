7b97a67b82e115bd150cb1859b7fca9cc1b554ab
Ivan <ivansergienko3211@gmail.com>
2021-04-19 20:31:37 +0200

    Lexer implementation

---
+++ src/main/kotlin/klox/lexer/Lexer.kt
@@ -0,0 +1,158 @@
+package klox.lexer
+
+import klox.Klox
+import java.util.*
+import kotlin.collections.HashMap
+
+class Lexer(private val source: String) {
+    private val tokens: MutableList<Token> = ArrayList()
+    private var start = 0
+    private var current = 0
+    private var line = 1
+    private val keywords = mapOf(
+        "and" to TokenType.AND,
+        "class" to TokenType.CLASS,
+        "else" to TokenType.ELSE,
+        "false" to TokenType.FALSE,
+        "for" to TokenType.FOR,
+        "fun" to TokenType.FUN,
+        "if" to TokenType.IF,
+        "nil" to TokenType.NIL,
+        "or" to TokenType.OR,
+        "print" to TokenType.PRINT,
+        "return" to TokenType.RETURN,
+        "super" to TokenType.SUPER,
+        "this" to TokenType.THIS,
+        "true" to TokenType.TRUE,
+        "var" to TokenType.VAR,
+        "while" to TokenType.WHILE
+    )
+
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd) {
+            // We are at the beginning of the next lexeme.
+            start = current
+            scanToken()
+        }
+        tokens.add(Token(TokenType.EOF, "", null, line))
+        return tokens
+    }
+
+    private fun scanToken() {
+        when (val c = advance()) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+            '!' -> addToken(if (match('=')) TokenType.BANG_EQUAL else TokenType.BANG)
+            '=' -> addToken(if (match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL)
+            '<' -> addToken(if (match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
+            '>' -> addToken(if (match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
+            '/' -> if (match('/')) {
+                // A comment goes until the end of the line.
+                while (peek() != '\n' && !isAtEnd) advance()
+            } else {
+                addToken(TokenType.SLASH)
+            }
+            ' ', '\r', '\t' -> {
+            }
+            '\n' -> line++
+            '"' -> string()
+            else -> when {
+                isDigit(c) -> number()
+                isAlpha(c) -> identifier()
+                else -> Klox.error(line, "Unexpected character.")
+            }
+        }
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) advance()
+        val text = source.substring(start, current)
+        var type = keywords[text]
+        if (type == null) type = TokenType.IDENTIFIER
+        addToken(type)
+    }
+
+    private fun isAlpha(c: Char): Boolean {
+        return c in 'a'..'z' ||
+                c in 'A'..'Z' ||
+                c == '_'
+    }
+
+    private fun isAlphaNumeric(c: Char): Boolean {
+        return isAlpha(c) || isDigit(c)
+    }
+
+    private fun isDigit(c: Char): Boolean {
+        return c in '0'..'9'
+    }
+
+    private fun number() {
+        while (isDigit(peek())) advance()
+
+        // Look for a fractional part.
+        if (peek() == '.' && isDigit(peekNext())) {
+            // Consume the "."
+            advance()
+            while (isDigit(peek())) advance()
+        }
+        addToken(TokenType.NUMBER, source.substring(start, current).toDouble())
+    }
+
+    private fun string() {
+        while (peek() != '"' && !isAtEnd) {
+            if (peek() == '\n') line++
+            advance()
+        }
+        if (isAtEnd) {
+            Klox.error(line, "Unterminated string.")
+            return
+        }
+
+        // The closing ".
+        advance()
+
+        // Trim the surrounding quotes.
+        val value = source.substring(start + 1, current - 1)
+        addToken(TokenType.STRING, value)
+    }
+
+    private fun peek(): Char {
+        return if (isAtEnd) '\u0000' else source[current]
+    }
+
+    private fun peekNext(): Char {
+        return if (current + 1 >= source.length) '\u0000' else source[current + 1]
+    }
+
+    private fun match(expected: Char): Boolean {
+        return when {
+            isAtEnd -> false
+            source[current] != expected -> false
+            else -> {
+                current++
+                true
+            }
+        }
+    }
+
+    private fun advance(): Char {
+        return source[current++]
+    }
+
+    private fun addToken(type: TokenType, literal: Any? = null) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+
+    private val isAtEnd: Boolean
+        get() = current >= source.length
+
+}
---
+++ src/main/kotlin/klox/lexer/Token.kt
@@ -0,0 +1,12 @@
+package klox.lexer
+
+data class Token(
+    val type: TokenType,
+    val lexeme: String,
+    val literal: Any?,
+    val line: Int
+) {
+    override fun toString(): String {
+        return "$type $lexeme $literal"
+    }
+}
